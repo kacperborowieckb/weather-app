f00f19ca37e799dcdfcdb6dc0649cdb47b24b612
Kacper Borowiec <kborowiec@griddynamics.com>
2024-08-27 12:14:17 +0200

    feat: mock layout for weather widget

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <template>
-
+    <header class="header">Header</header>
+    <main class="main">
+        <WeatherWidget />
+    </main>
+    <footer class="footer">Footer</footer>
 </template>
 
 <script setup lang="ts">
-
+import WeatherWidget from './components/WeatherWidget.vue'  
 </script>
 
 
-<style scoped></style>
+<style scoped>
+.header {
+    height: 72px;
+}
+
+.main {
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+}
+
+.footer {
+    height: 72px;
+}
+</style>
---
+++ src/components/WeatherWidget.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>Weather widdget</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+
+<style scoped></style>
