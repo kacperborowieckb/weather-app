1c1047e989a10d903170622a664c9479a5a70b2f
Kacper Borowiec <kborowiec@griddynamics.com>
2024-08-30 22:46:48 +0200

    feat: changed useFetch to use axios, added isLoading ref, returning function that trigger request

--- src/composables/useFetch.ts
+++ src/composables/useFetch.ts
@@ -1,27 +1,48 @@
+import axios, {
+  type Method,
+  type AxiosRequestConfig,
+  type AxiosResponse,
+} from "axios";
 import { ref, type Ref } from "vue";
+
+type FetchData = (method: Method, payload?: any) => void;
 
 type UseFetchReturnType<T> = {
   data: Ref<T | null>;
   error: Ref<Error | null>;
+  isLoading: Ref<boolean>;
+  fetchData: FetchData;
 };
 
-export async function useFetch<T>(url: string): Promise<UseFetchReturnType<T>> {
+export function useFetch<T>(
+  url: string,
+  config?: AxiosRequestConfig
+): UseFetchReturnType<T> {
   const data: Ref<T | null> = ref(null);
   const error: Ref<Error | null> = ref(null);
+  const isLoading: Ref<boolean> = ref(false);
 
-  try {
-    const res = await fetch(url);
+  const fetchData: FetchData = async (method = "GET", payload) => {
+    error.value = null;
+    isLoading.value = true;
 
-    if (!res.ok) {
-      throw new Error(`Something went wrong ${res.status}`);
+    try {
+      const res: AxiosResponse<T> = await axios.request({
+        url,
+        method,
+        data: payload,
+        ...config,
+      });
+
+      data.value = res.data;
+    } catch (e) {
+      if (e instanceof Error) {
+        error.value = e;
+      }
+    } finally {
+      isLoading.value = false;
     }
+  };
 
-    data.value = await res.json();
-  } catch (e) {
-    if (e instanceof Error) {
-      error.value = e;
-    }
-  }
-
-  return { data, error };
+  return { data, error, isLoading, fetchData };
 }
