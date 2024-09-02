a2adb514a672cb057182f0ad174c83c1d51a18cf
Kacper Borowiec <kborowiec@griddynamics.com>
2024-09-02 12:33:55 +0200

    feat: added getLocation util, checking for access to geolocation and displaying message

--- src/components/WeatherWidget.vue
+++ src/components/WeatherWidget.vue
@@ -1,32 +1,39 @@
 <template>
   <div class="weather-widget">
-    <WeatherWidgetPlace
-      v-bind="weatherData.location"
-      :date="selectedDayData?.date ?? 'Today'"
-    />
-    <WeatherWidgetCurrentData v-bind="selectedDayData" />
-    <WeatherWidgetForecast
-      :forecastData="weatherData.forecast"
-      :selectedDay
-      @handleDayChange="handleDayChange"
-    />
+    <WeatherWidgetMessage v-if="geoLocationError" :message="geoLocationError" />
+    <template v-else>
+      <WeatherWidgetPlace
+        v-bind="weatherData.location"
+        :date="selectedDayData?.date ?? 'Today'"
+      />
+      <WeatherWidgetCurrentData v-bind="selectedDayData" />
+      <WeatherWidgetForecast
+        :forecastData="weatherData.forecast"
+        :selectedDay
+        @handleDayChange="handleDayChange"
+      />
+    </template>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { computed, ref, onMounted } from "vue";
 
 import { mockWeatherResponseData } from "@/mocks/mockWeatherDataResponse";
 import { mapWeatherData } from "@/utils/dataMappers";
 import { getCurrentDate } from "@/helpers/getCurrentDate";
+import { Coordinates, getLocation } from "@/helpers/getLocation";
 
 import WeatherWidgetPlace from "./WeatherWidgetPlace.vue";
 import WeatherWidgetCurrentData from "./WeatherWidgetCurrentData.vue";
 import WeatherWidgetForecast from "./WeatherWidgetForecast.vue";
+import WeatherWidgetMessage from "./WeatherWidgetMessage.vue";
 
 const currentDate = getCurrentDate();
 
 const selectedDay = ref<string>(currentDate);
+const coords = ref<Coordinates | null>(null);
+const geoLocationError = ref<string | null>(null);
 
 const weatherData = computed(() => mapWeatherData(mockWeatherResponseData));
 
@@ -45,6 +52,14 @@ const handleDayChange = (newDay: string) => {
     selectedDay.value = newDay;
   }
 };
+
+onMounted(async () => {
+  try {
+    coords.value = await getLocation();
+  } catch (err) {
+    geoLocationError.value = err as string;
+  }
+});
 </script>
 
 <style scoped lang="scss">
---
+++ src/components/WeatherWidgetMessage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">
+    <p>{{ props.message }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ message: string }>();
+</script>
+
+<style lang="scss">
+.wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  aspect-ratio: 2.5/1;
+  text-align: center;
+}
+</style>
